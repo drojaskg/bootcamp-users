8006b0b6de678746d38b793875f67a790936ca05
David Rojas <drojas@konradgroup.com>
2020-10-14 20:58:40 -0600

    first commit

--- app.js
+++ app.js
@@ -1,25 +1,33 @@
 const http = require('http');
-const port = 3000;
-const server = http.createServer();
+const express = require('express');
+const app = express();
 const url = require('url');
 var data = require('./data');
 
 const PORT = process.env.PORT || 3000;
 
-server.on('request', async (req, res) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'application/json');
+// server.on('request', async (req, res) => {
+//     res.statusCode = 200;
+//     res.setHeader('Content-Type', 'application/json');
 
-    var url_parts = url.parse(req.url, true);
-    var query = url_parts.query;
+//     var url_parts = url.parse(req.url, true);
+//     var query = url_parts.query;
 
-    console.log(query);
+//     console.log(query);
 
-    let user = data.getUser(query.email, query.password);
+//     let user = data.getUser(query.email, query.password);
 
-    res.end(JSON.stringify(user));
-});
+//     res.end(JSON.stringify(user));
+// });
 
-server.listen(PORT, () => {
-    console.log(`Server running`);
-});
+app.get('/', (req, res) => {
+    console.log(req.query);
+
+    let user = data.getUser(req.query.email, req.query.password);
+
+    res.json(user)
+})
+
+app.listen(PORT, () => {
+    console.log(`Example app listening at http://localhost:${PORT}`)
+})
