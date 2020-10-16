1f510d8302e591f98d13c47e18bebe80ba31f519
David Rojas <drojas@konradgroup.com>
2020-10-16 02:22:43 -0600

    videos

--- app.js
+++ app.js
@@ -15,6 +15,22 @@ app.get('/', (req, res) => {
     res.status(200).json(user)
 })
 
+app.get('/videos', (req, res) => {
+    console.log(req.query);
+
+    let videos = data.getVideos(req.query.search);
+
+    res.status(200).json(videos)
+})
+
+app.get('/video', (req, res) => {
+    console.log(req.query);
+
+    let video = data.getVideo(req.query.id);
+
+    res.status(200).json(video)
+})
+
 app.listen(PORT, () => {
     console.log(`Example app listening at http://localhost:${PORT}`)
 })
--- data.js
+++ data.js
@@ -1,4 +1,5 @@
 var Users = require('./users.json');
+var Videos = require('./videos.json');
 
 function getUser(email, password) {
     if (!email || !password) {
@@ -14,4 +15,36 @@ function getUser(email, password) {
     return {status: 400, message: "Invalid user!"}
 }
 
-module.exports = {getUser};
+function getVideo(id) {
+    if (!id) {
+        return {status: 400, message: "Invalid video id!"}
+    }
+
+    const video = Videos.find(v => v.id === id);
+
+    if (video) {
+        return {status: 200, video}
+    }
+
+    return {status: 400, message: "Video not found!"}
+}
+
+const getVideoShortData = (video) => ({id: video.id, title: video.title, description: video.description, poster: video.poster});
+
+function getVideos(search) {
+    if (!search || search === 'undefined') {
+        return {status: 200, videos: Videos.map(v => getVideoShortData(v))}
+    }
+
+    search = search.toLowerCase();
+
+    const videos = Videos.filter(v => v.title.toLowerCase().includes(search) || v.description.toLowerCase().includes(search)).map(v => getVideoShortData(v));
+
+    return {status: 200, videos}
+}
+
+module.exports = {
+    getUser,
+    getVideo,
+    getVideos
+};
